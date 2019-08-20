55cde91909fc0fcee68c3ff34527c7e1dbae71e9
fatimahSheikho <40785852+fatimahSheikho@users.noreply.github.com>
2019-08-21 00:36:57 +0300

    Add files via upload

---
+++ Arrays.py
@@ -0,0 +1,13 @@
+friends = [ "Sara" , "Fatimah", "Kiven"]
+numbers = [0,2,3,4,5,6,7]
+friends.extend(numbers)
+friends.pop()
+friends.append("TAMA")
+friends.insert(4,"HALA")
+numbers.sort()
+friends3=friends.copy()
+print(friends)
+print(friends3)
+print(numbers)
+ArrayOfTuples = [(2,4) , (3,5) , (8,9) , (9,19)]
+print(ArrayOfTuples)
