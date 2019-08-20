78e783f3e57a697aeed5e945ee2b602ae2c6b440
fatimahSheikho <40785852+fatimahSheikho@users.noreply.github.com>
2019-08-21 01:08:45 +0300

    Add files via upload

---
+++ Student.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self, name , major , gpa , accepted):
+        self.name=name
+        self.major=major
+        self.gpa=gpa
+        self.accepted=accepted
+
+        
