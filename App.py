0bdd7e6ac0eb258526b227463aa2d0db152d07c1
fatimahSheikho <40785852+fatimahSheikho@users.noreply.github.com>
2019-08-21 00:37:17 +0300

    Add files via upload

---
+++ App.py
@@ -0,0 +1,14 @@
+print("hello world")
+print("this is my first python program")
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+Name = "Sara"
+age = 23
+istrue= True
+print("someone called\n " + Name + "\n her age is  \n "+ str(age) +"." )
+print(Name.upper())
+print(Name.isupper())
+print(len(Name))
+print(Name.replace("Sar","Fat"))
