151a8d78c23bec870f50a8b6b17188287e1e5e2e
fatimahSheikho <40785852+fatimahSheikho@users.noreply.github.com>
2019-08-21 00:36:00 +0300

    Add files via upload

---
+++ Inputs.py
@@ -0,0 +1,7 @@
+name = ""
+name  = input("Enter your name:")
+age  = input("Enter your age:")
+print("Hello" + name + "and your age is "+age)
+num = input("Enter a number:")
+num2 = input("Enter another number")
+print("The sum of the numbers is: "+str (float(num) + float(num2)))
